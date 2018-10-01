77d3ebc1f7b1725d21034dad877fb949d12e84c2
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-01 12:43:09 +0300

    Создан макет с кнопкой "сделать фотографию" и к нему привязан метод makePhoto.

--- app/src/main/java/com/omelchenkoaleks/cameraandroid/StartCameraActivity.java
+++ app/src/main/java/com/omelchenkoaleks/cameraandroid/StartCameraActivity.java
@@ -2,6 +2,7 @@ package com.omelchenkoaleks.cameraandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartCameraActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class StartCameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_camera);
+    }
+
+    public void makePhoto(View view) {
+
     }
 }
